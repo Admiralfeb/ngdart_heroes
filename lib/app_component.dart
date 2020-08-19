660d9840ecd7123e57568f477276c3c745eedc38
Zachary Bryant <Zachary.Bryant@wolterskluwer.com>
2020-08-19 17:10:45 -0500

    init

---
+++ lib/app_component.dart
@@ -0,0 +1,22 @@
+import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+
+import 'src/hero.dart';
+import 'src/mock_heroes.dart';
+
+// AngularDart info: https://angulardart.dev
+// Components info: https://angulardart.dev/components
+
+@Component(
+  selector: 'my-app',
+  styleUrls: ['app_component.css'],
+  templateUrl: 'app_component.html',
+  directives: [coreDirectives, formDirectives],
+)
+class AppComponent {
+  final title = 'Tour of Heroes';
+  List<Hero> heroes = mockHeroes;
+  Hero selected;
+
+  void onSelect(Hero hero) => selected = hero;
+}
